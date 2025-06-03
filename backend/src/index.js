8bb2f03807d486192c665703debea7600ffbb185
arpitshukla9 <arpitshukla0901@gmail.com>
2025-06-03 06:56:32 +0530

    fix: dynamic routes error

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,6 +36,7 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  console.log(__dirname)
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
