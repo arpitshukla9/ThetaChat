a0770b09a81fd7fa5545358f84881b96595ad29d
arpitshukla9 <arpitshukla0901@gmail.com>
2025-06-03 18:57:30 +0530

    Fix login issue

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-if (!import.meta.env.VITE_API_URL) {
-  throw new Error("VITE_API_URL is missing — check .env files!");
-}
-
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
