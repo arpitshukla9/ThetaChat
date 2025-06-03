e39d3ddb539d34b59426e182596b2f7185ee0e04
arpitshukla9 <arpitshukla0901@gmail.com>
2025-06-03 16:37:51 +0530

    Fix login issue, update environment variables, and improve axios config

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,13 +2,11 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
-const BASE_URL = import.meta.env.MODE === "development"
-  ? "http://localhost:5001/api"
-  : "https://thetachat.onrender.com/api";
 
-const SOCKET_URL = import.meta.env.MODE === "development"
-  ? "http://localhost:5001"
-  : "https://thetachat.onrender.com";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5001/api";
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:5001";
+
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
